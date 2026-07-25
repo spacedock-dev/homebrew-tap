f59d62cb47c813cc8db41b928e0cac921d6ce7c3
CL Kao <clkao@datarecce.io>
2026-07-25 20:47:15 +0800

    release: update Subspace formula to 0.10.0-beta.5

--- Formula/subspace-beta.rb
+++ Formula/subspace-beta.rb
@@ -1,28 +1,28 @@
 class SubspaceBeta < Formula
   desc "Native Subspace Review v1 TUI"
   homepage "https://github.com/spacedock-dev/subspace-beta"
-  version "0.10.0-beta.4"
+  version "0.10.0-beta.5"
   license :cannot_represent
 
   on_macos do
     on_arm do
-      url "https://github.com/spacedock-dev/subspace-beta/releases/download/v0.10.0-beta.4/subspace_0.10.0-beta.4_darwin_arm64.tar.gz"
-      sha256 "2c8f2ba51db976fbbc8344b6f68d3d54aef29d611c587e828b4b9f415afff364"
+      url "https://github.com/spacedock-dev/subspace-beta/releases/download/v0.10.0-beta.5/subspace_0.10.0-beta.5_darwin_arm64.tar.gz"
+      sha256 "a4d0dc5beb48c242b5266e2ff070ac0c05e8be7421f7954304c41f015abe27e2"
     end
     on_intel do
-      url "https://github.com/spacedock-dev/subspace-beta/releases/download/v0.10.0-beta.4/subspace_0.10.0-beta.4_darwin_amd64.tar.gz"
-      sha256 "58931442dede636eca14394df87f7e8b82b269b837ea9ae80b5c59932838d5f2"
+      url "https://github.com/spacedock-dev/subspace-beta/releases/download/v0.10.0-beta.5/subspace_0.10.0-beta.5_darwin_amd64.tar.gz"
+      sha256 "14ceb73aec96634ead403e0d244854c0d8dbb22f409494e2756c1265ade03b60"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/spacedock-dev/subspace-beta/releases/download/v0.10.0-beta.4/subspace_0.10.0-beta.4_linux_arm64.tar.gz"
-      sha256 "de94847e126a619013f29e84a757daef53722917b4851aa515e95f95059438ea"
+      url "https://github.com/spacedock-dev/subspace-beta/releases/download/v0.10.0-beta.5/subspace_0.10.0-beta.5_linux_arm64.tar.gz"
+      sha256 "6af36f092c4d54c65015901a18d7d365a5234ab162f16e57a233e35c3bba7e3a"
     end
     on_intel do
-      url "https://github.com/spacedock-dev/subspace-beta/releases/download/v0.10.0-beta.4/subspace_0.10.0-beta.4_linux_amd64.tar.gz"
-      sha256 "b73251881d4be77469410f326ffd2aab7afb2571ebe9e15bb4cdeb9f63b9b684"
+      url "https://github.com/spacedock-dev/subspace-beta/releases/download/v0.10.0-beta.5/subspace_0.10.0-beta.5_linux_amd64.tar.gz"
+      sha256 "37b6ea0e068f2670a7eb406088ec8f3ab2d71a679a76f4f951b64f91f38ba145"
     end
   end
 
@@ -32,7 +32,7 @@ class SubspaceBeta < Formula
   end
 
   test do
-    assert_equal "0.10.0-beta.4", shell_output("#{bin}/subspace-tui --version").strip
-    assert_equal "0.10.0-beta.4", shell_output("#{bin}/sr --version").strip
+    assert_equal "0.10.0-beta.5", shell_output("#{bin}/subspace-tui --version").strip
+    assert_equal "0.10.0-beta.5", shell_output("#{bin}/sr --version").strip
   end
 end
